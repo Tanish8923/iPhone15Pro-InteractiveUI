734fd07459bae9188e9a9496447855312d2c32a1
Tanish8923 <tg6398119861@gmail.com>
2024-11-23 01:10:08 +0530

    modified the project to meet the requirements

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "tg-lo",
-    project: "javascript-react"
+    project: "javascript-react",
+    authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
   })],
 
   build: {
